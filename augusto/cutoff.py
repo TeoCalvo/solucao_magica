177586d4f44e7ae48a6b57d9048cb062a691cb9f
Téo Calvo <teocalvo2@gmail.com>
2020-05-29 22:00:03 -0300

    Tabela de lift, gain e taxa de resposta

--- augusto/cutoff.py
+++ augusto/cutoff.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import numpy as np
 import sqlalchemy
 from sklearn import tree
 from sklearn import preprocessing
 from sklearn import metrics
+import matplotlib.pyplot as plt
 
 # Abre conecão com banco
 dbpath = "/home/teo/Documentos/pessoais/projetos/ensino/projetos_twitch/maratona_ds/data/olist.db"
@@ -27,4 +29,21 @@ df_full = df_full[ model['features_fit'] ]
 df["proba"] = model["model"].predict_proba( df_full )[:,1]
 df_analysis = df[['flag_churn', 'proba']].copy()
 
-metrics.roc_auc_score( df_analysis["flag_churn"], df_analysis["proba"] )
+def create_lift_table( y_true, proba, k ):
+    percentis = np.linspace(0,100, int(100/k +1) )
+    vl_percentis = np.percentile(proba, percentis)
+    df_analysis["percentil"] = df_analysis["proba"].apply( lambda x: percentis[np.searchsorted( vl_percentis, x )] )
+    tb_cutoff = df_analysis.groupby(["percentil"]).agg({"flag_churn":["count","sum", "mean"]})
+    tb_cutoff.columns = tb_cutoff.columns.droplevel()
+    tb_cutoff = tb_cutoff.reset_index().sort_values(by="percentil", ascending=False)
+
+    tb_cutoff["cum_count"] = tb_cutoff["count"].cumsum()
+    tb_cutoff["cum_sum"] = tb_cutoff["sum"].cumsum()
+    tb_cutoff["cum_mean"] = tb_cutoff["cum_sum"] / tb_cutoff["cum_count"]
+
+    tb_cutoff["gain"] = tb_cutoff["cum_mean"] / tb_cutoff["cum_mean"].min()
+    tb_cutoff["tx_resposta"] = tb_cutoff["cum_sum"] / tb_cutoff["cum_sum"].max()
+
+    return tb_cutoff
+
+np.percentile( df["proba"], 80 )
